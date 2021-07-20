ea78713200211cf6aa470d3de4af2fbfec90a748
Dan Menjivar <41385232+danmenjivar@users.noreply.github.com>
2021-07-20 10:57:23 -0700

    Moved over to Pug from hbs

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config(); // import env for logging in to mongo
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
-require('dotenv').config(); // import env for logging in to mongo
 
 var app = express();
 
@@ -21,7 +21,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
