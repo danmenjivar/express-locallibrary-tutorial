781ba3041c8ca684955515d0e11e9fbcdeef62d0
Dan Menjivar <41385232+danmenjivar@users.noreply.github.com>
2021-07-23 09:51:35 -0700

    Prepare for deploy: comrpession

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config(); // import env for logging in to mongo
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
-
+var compression = require('compression');
 var app = express();
 
 // Set up mongoose connection and bind error events
@@ -27,6 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
